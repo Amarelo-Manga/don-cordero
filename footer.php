5c12dbb01f4ea7c328b3e86a6874e8756a83046b
Walace Felix <walacefex01@gmail.com>
2018-01-30 15:03:52 -0200

    mudanças de links

--- footer.php
+++ footer.php
@@ -44,7 +44,4 @@ Domingos e Feriados das 12h00 às 18h00</div></li>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
     <script src="<?php echo get_template_directory_uri()?>/jQueryAssets/jquery.ui-1.10.4.tabs.min.js"></script>
-<link href="<?php echo get_template_directory_uri()?>/jQueryAssets/jquery.ui.core.min.css" rel="stylesheet" type="text/css">
-<link href="<?php echo get_template_directory_uri()?>/jQueryAssets/jquery.ui.theme.min.css" rel="stylesheet" type="text/css">
-<link href="<?php echo get_template_directory_uri()?>/jQueryAssets/jquery.ui.tabs.min.css" rel="stylesheet" type="text/css">
 </html>
